512f4933a6bc590e20bb11303bfffc292a6a7cda
Alvine Allan <alvizallaz@gmail.com>
2023-10-23 08:40:31 +0300

    Completed Assignment

--- index.js
+++ index.js
@@ -1,1 +1,7 @@
 // Your code goes here
+  document.addEventListener("DOMContentLoaded", function() {
+  console.log("The DOM has loaded");
+  const text = document.querySelector("p");
+text.textContent = "This is really cool!"
+
+});
